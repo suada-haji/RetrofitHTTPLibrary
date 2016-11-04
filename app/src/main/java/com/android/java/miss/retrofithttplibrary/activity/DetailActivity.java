a4af64fac3bd3656d635b7fbeca792c73d11eff8
“Suada <suada.haji@andela.com>
2016-11-04 13:02:25 +0300

    Add Detail Fragment for Detail Activity

--- app/src/main/java/com/android/java/miss/retrofithttplibrary/activity/DetailActivity.java
+++ app/src/main/java/com/android/java/miss/retrofithttplibrary/activity/DetailActivity.java
@@ -4,62 +4,32 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.android.java.miss.retrofithttplibrary.R;
-import com.android.java.miss.retrofithttplibrary.model.Movie;
-import com.android.java.miss.retrofithttplibrary.rest.ApiClient;
-import com.android.java.miss.retrofithttplibrary.rest.ApiInterface;
-import com.squareup.picasso.Picasso;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import com.android.java.miss.retrofithttplibrary.fragments.DetailFragment;
 
 public class DetailActivity extends AppCompatActivity {
-  private TextView movieTitle;
-  private ImageView movieImage;
-  private TextView movieRating;
-  private TextView releaseDate;
-  private TextView overview;
+  public static final String MOVIE_ID = "MOVIE_ID";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_detail);
+    setContentView(R.layout.activity_movie_detail);
     final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
-    getSupportActionBar().setTitle("Detail Activity");
+    getSupportActionBar().setTitle("    Movie");
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    if (savedInstanceState == null) {
+      DetailFragment fragment = new DetailFragment();
+      String id = getIntent().getStringExtra(MainActivity.MOVIE_BUNDLE);
+      Bundle bundle = new Bundle();
+      bundle.putString(MOVIE_ID, id);
+      fragment.setArguments(bundle);
 
-    movieTitle = (TextView) findViewById(R.id.movie_title);
-    movieImage = (ImageView) findViewById(R.id.movie_image);
-    movieRating = (TextView) findViewById(R.id.rating);
-    releaseDate = (TextView) findViewById(R.id.release_date);
-    overview = (TextView) findViewById(R.id.overview);
-
-
-   String id = getIntent().getStringExtra(MainActivity.MOVIE_BUNDLE);
-    int movieId = Integer.parseInt(id);
-    ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
-    Call<Movie> call = apiService.getMovieDetails(movieId, MainActivity.API_KEY);
-    call.enqueue(new Callback<Movie>() {
-      @Override
-      public void onResponse(Call<Movie> call, Response<Movie> response) {
-        movieTitle.setText(response.body().getTitle());
-        Picasso.with(DetailActivity.this).load("http://image.tmdb.org/t/p/original/" + response.body().getPosterPath()).into(movieImage);
-        movieRating.setText(Double.toString(response.body().getVoteAverage()));
-        releaseDate.setText(response.body().getReleaseDate());
-        overview.setText(response.body().getOverview());
-      }
-
-      @Override
-      public void onFailure(Call<Movie> call, Throwable t) {
-
-      }
-    });
-
+      getFragmentManager().beginTransaction()
+              .add(R.id.detailContainer, fragment)
+              .commit();
+    }
   }
 
   @Override
---
+++ app/src/main/java/com/android/java/miss/retrofithttplibrary/fragments/DetailFragment.java
@@ -0,0 +1,78 @@
+package com.android.java.miss.retrofithttplibrary.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.java.miss.retrofithttplibrary.R;
+import com.android.java.miss.retrofithttplibrary.activity.DetailActivity;
+import com.android.java.miss.retrofithttplibrary.activity.MainActivity;
+import com.android.java.miss.retrofithttplibrary.model.Movie;
+import com.android.java.miss.retrofithttplibrary.rest.ApiClient;
+import com.android.java.miss.retrofithttplibrary.rest.ApiInterface;
+import com.squareup.picasso.Picasso;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class DetailFragment extends Fragment{
+
+  private TextView movieTitle;
+  private ImageView movieImage;
+  private TextView movieRating;
+  private TextView releaseDate;
+  private TextView overview;
+  private String id;
+
+  public DetailFragment() {
+
+  }
+
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    Bundle bundle =getArguments();
+    if (bundle != null) {
+      id = bundle.getString(DetailActivity.MOVIE_ID);
+    }
+  }
+
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.activity_detail, container, false);
+
+    movieTitle = (TextView) view.findViewById(R.id.movie_title);
+    movieImage = (ImageView) view.findViewById(R.id.movie_image);
+    movieRating = (TextView) view.findViewById(R.id.rating);
+    releaseDate = (TextView) view.findViewById(R.id.release_date);
+    overview = (TextView) view.findViewById(R.id.overview);
+
+    int movieId = Integer.parseInt(id);
+    ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+    Call<Movie> call = apiService.getMovieDetails(movieId, MainActivity.API_KEY);
+    call.enqueue(new Callback<Movie>() {
+      @Override
+      public void onResponse(Call<Movie> call, Response<Movie> response) {
+        movieTitle.setText(response.body().getTitle());
+        Picasso.with(getContext()).load("http://image.tmdb.org/t/p/original/" + response.body().getPosterPath()).into(movieImage);
+        movieRating.setText(Double.toString(response.body().getVoteAverage()));
+        releaseDate.setText(response.body().getReleaseDate());
+        overview.setText(response.body().getOverview());
+      }
+
+      @Override
+      public void onFailure(Call<Movie> call, Throwable t) {
+
+      }
+    });
+    return view;
+  }
+}
